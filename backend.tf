ddc6349d952629677db2b6596034f238dd958029
korupon <68062483+korupon@users.noreply.github.com>
2025-04-14 21:58:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "crif"
-    storage_account_name = "terraformcrif2"
+    storage_account_name = "terraformcrif3"
     container_name       = "tfstatecrif"
     key                  = "default.terraform.tfstate"
   }
